7545524f7591816609d5e45f50f5aed794ee8c90
your name here <jchen143@fordham.edu>
2025-01-25 16:33:43 -0600

    add device functionality

--- src/base-components/Form/DeviceForm.tsx
+++ src/base-components/Form/DeviceForm.tsx
@@ -8,40 +8,43 @@ import { Device } from 'src/util/devices-util';
 interface DeviceFormProps {
   defaultValues?: Device;
   onSubmit: (data: Device) => void;
-  closeForm?: () => void;
+  closeForm: () => void;
 }
 
 const DeviceForm: React.FC<DeviceFormProps> = (props) => {
   const { defaultValues, onSubmit, closeForm } = props;
-  const { systemName, deviceType, deviceCapacity } = defaultValues ?? {};
+  const { system_name, type, hdd_capacity } = defaultValues ?? {};
 
-  const [formData, setFormData] = React.useState<Device>(defaultValues ?? { systemName: "", deviceType: "", deviceCapacity: 0 });
+  const [formData, setFormData] = React.useState<Device>(defaultValues ?? { system_name: "", type: "", hdd_capacity: 0 });
 
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({ ...formData, systemName: e.target.value });
+    setFormData({ ...formData, system_name: e.target.value });
   };
 
   const handleDeviceTypeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({ ...formData, deviceType: e.target.value });
+      setFormData({ ...formData, type: e.target.value });
   };
 
   const handleDeviceCapacityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({ ...formData, deviceCapacity: Number(e.target.value) });
+    setFormData({ ...formData, hdd_capacity: Number(e.target.value) });
   };
 
-  const systenNameField = <input name="systemName" defaultValue={systemName} onChange={handleNameChange}/>
-  const deviceTypeField = <input name="deviceType" defaultValue={deviceType} onChange={handleDeviceTypeChange} />
+  const systenNameField = <input name="systemName" defaultValue={system_name} onChange={handleNameChange}/>
+  const deviceTypeField = <input name="deviceType" defaultValue={type} onChange={handleDeviceTypeChange} />
   const deviceCapacityField = <input
         name="deviceCapacity"
         type="number"
-        defaultValue={deviceCapacity}
+        defaultValue={hdd_capacity}
           onChange={handleDeviceCapacityChange}
         />
 
   const fields = [systenNameField, deviceTypeField, deviceCapacityField];
 
-  const cancelButton = <Button text="Cancel" buttonType={ButtonType.NEUTRAL} onClick={() => {closeForm?.()}} />;
-  const submitButton = <Button text="Add Device" buttonType={ButtonType.PRIMARY} onClick={() => {onSubmit(formData)}} />;
+  const cancelButton = <Button text="Cancel" buttonType={ButtonType.NEUTRAL} onClick={() => {closeForm()}} />;
+  const submitButton = <Button text="Add Device" buttonType={ButtonType.PRIMARY} onClick={() => {
+    onSubmit(formData)
+    closeForm()
+  }} />;
 
   const actionButtons = [cancelButton, submitButton];
 
