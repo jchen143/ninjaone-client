777df2d8f5a234c09f10d462f9d4461c284fd5a8
your name here <jchen143@fordham.edu>
2025-01-27 12:33:24 -0600

    wip get the alignment of items right

--- src/components/DataTable/DevicesTable/DeviceRowItem.tsx
+++ src/components/DataTable/DevicesTable/DeviceRowItem.tsx
@@ -1,8 +1,13 @@
 import React from "react"; 
 import Modal from "src/base-components/Modal/Modal";
-import { Device } from "src/types/types";
+import { Device, DeviceTypes } from "src/types/types";
 import EditDeviceForm from "src/components/DataTable/DevicesTable/EditDeviceForm";
 import DeleteDeviceForm from "src/components/DataTable/DevicesTable/DeleteDeviceForm";
+
+import "./DeviceRowItem.scss";
+import AppleLogo from "src/media/AppleLogo";
+import LinuxLogo from "src/media/LinuxLogo";
+import WindowsLogo from "src/media/WindowsLogo";
 
 interface DeviceItemProps {
   device: Device;
@@ -22,18 +27,58 @@ const DeviceRowItem: React.FC<DeviceItemProps> = (props) => {
     </>
   )
 
+  const getDeviceIcon = () => {
+    switch(device.type) {
+      case DeviceTypes.WINDOWS:
+        return <WindowsLogo />
+      case DeviceTypes.LINUX:
+        return <LinuxLogo />
+      case DeviceTypes.MAC:
+        return <AppleLogo />
+    }
+  };
+
+  const getDeviceTypeSubheaderString = () => {
+    switch(device.type) {
+      case DeviceTypes.WINDOWS:
+        return "Windows workstation"
+      case DeviceTypes.LINUX:
+        return "Linux workstation"
+      case DeviceTypes.MAC:
+        return "Apple workstation"
+    }
+  }
+
+  const getDeviceHddCapacityString = () => {
+    return `${device.hdd_capacity} GB`
+  }
+
   return(
-    <div className="device-row-item">
-      <div key={device.id}>{device.system_name} {device.type} {device.hdd_capacity}</div>
-      <button onClick={(e) => setDeviceOptionsOpen(!deviceOptionsOpen)}></button>
-      {deviceOptionsOpen && editAndDelete}
+    <>
+      <div className="device-row-item">
+        <div className="device-row-item__left">
+          <div className="device-row-item__left__header">
+            <div className="device-row-item__left__header__device-icon">{getDeviceIcon()}</div>
+            <div className="device-row-item__left__header__device-name">{device.system_name}</div>
+          </div>
+          <div className="device-row-item__left__body">
+            <div className="device-row-item__left__body__device-subheader">{`${getDeviceTypeSubheaderString()} - ${getDeviceHddCapacityString()}`}</div>
+          </div>
+        </div>
+
+        <div className="device-row-item__right">
+          <button onClick={(e) => setDeviceOptionsOpen(!deviceOptionsOpen)}></button>
+          {deviceOptionsOpen && editAndDelete}
+        </div>
+      </div>
+
       <Modal isOpen={deviceEditModalOpen} onClose={() => { setDeviceEditModalOpen(false) }} title="Edit Device">
-        <EditDeviceForm closeForm={() => {setDeviceEditModalOpen(false)}} deviceId={device.id}/>
+        <EditDeviceForm closeForm={() => { setDeviceEditModalOpen(false) }} deviceId={device.id} />
       </Modal>
-      <Modal isOpen={deviceDeleteModalOpen} onClose={() => { setDeviceDeleteModalOpen(false)}} title="Delete device?">
-        <DeleteDeviceForm deviceId={device.id} closeForm={() => setDeviceDeleteModalOpen(false)}/>
+      <Modal isOpen={deviceDeleteModalOpen} onClose={() => { setDeviceDeleteModalOpen(false) }} title="Delete device?">
+        <DeleteDeviceForm deviceId={device.id} closeForm={() => setDeviceDeleteModalOpen(false)} />
       </Modal>
-    </div>
+    </>
   )
 }
 
