0eb78c21a6c5cf35fa25506169e29420a4a91ade
your name here <jchen143@fordham.edu>
2025-01-28 22:48:40 -0600

    modal styling

--- src/base-components/Form/DeviceForm.tsx
+++ src/base-components/Form/DeviceForm.tsx
@@ -4,7 +4,7 @@ import Button, { ButtonType } from 'src/base-components/Button/Button';
 import Form from 'src/base-components/Form/Form';
 import { Device } from 'src/types/types';
 
-
+import './DeviceForm.scss';
 
 interface DeviceFormProps {
   defaultValues?: Device;
@@ -31,29 +31,45 @@ const DeviceForm: React.FC<DeviceFormProps> = ({isDelete = false, ...props}) => 
     setFormData({ ...formData, hdd_capacity: Number(e.target.value) });
   };
 
-  const systenNameField = <input name="systemName" defaultValue={system_name} onChange={handleNameChange}/>
-  const deviceTypeField = <input name="deviceType" defaultValue={type} onChange={handleDeviceTypeChange} />
-  const deviceCapacityField = <input
+  const systenNameField = (
+    <div className="form__field">
+      <label>System name *</label>
+      <input name="systemName" defaultValue={system_name} onChange={handleNameChange}/>
+    </div>
+  )
+  const deviceTypeField = (
+    <div className="form__field">
+      <label>Device type *</label>
+      <input name="deviceType" defaultValue={type} onChange={handleDeviceTypeChange} />
+    </div>
+  )
+  const deviceCapacityField = (
+    <div className="form__field">
+      <label>HDD capacity (GB)*</label>
+      <input
         name="deviceCapacity"
         type="number"
         defaultValue={hdd_capacity}
-          onChange={handleDeviceCapacityChange}
+        onChange={handleDeviceCapacityChange}
         />
-
+    </div>
+  )
   const fields = isDelete ? 
     [<div>{`You are about to delete the device ${defaultValues?.system_name}. This action cannot be undone`}</div>] : 
     [systenNameField, deviceTypeField, deviceCapacityField];
 
-  const cancelButton = <Button text="Cancel" buttonType={ButtonType.NEUTRAL} onClick={() => {closeForm()}} />;
+  const cancelButton = <Button text="Cancel" buttonType={ButtonType.NEUTRAL} onClick={() => {closeForm()}} additionalClassNames={["cancel-button"]} />;
   const submitButton = isDelete ? 
     <Button text="Delete" buttonType={ButtonType.ALERT} onClick={() => {
       onSubmit(formData)
       closeForm()
-    }} /> : 
+    }} 
+      additionalClassNames={["delete-button"]}
+    /> : 
     <Button text="Submit" buttonType={ButtonType.PRIMARY} onClick={() => {
       onSubmit(formData)
       closeForm()
-    }} />;
+    }} additionalClassNames={["submit-button"]} />;
 
   const actionButtons = [cancelButton, submitButton];
 
--- src/base-components/Modal/Modal.tsx
+++ src/base-components/Modal/Modal.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Modal.scss';
+import Xout from 'src/media/Xout';
 
 interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
   title: string;
   children: React.ReactNode;
+  additionalClassNames?: string[];
 }
 
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, children }) => {
+const Modal: React.FC<ModalProps> = (props) => {
+  const { isOpen, onClose, title, children, additionalClassNames } = props 
 
   return (isOpen ? ReactDOM.createPortal(
     <div className="modal-background">
-      <div className="modal">
+      <div className={"modal" + (additionalClassNames ? ` ${additionalClassNames.join(" ")}` : "")}>
         <div className="modal__header">
           <h2>{title}</h2>
-          <button className="modal__close" onClick={onClose}>×</button>
+          <button className="modal__close" onClick={onClose}><Xout /></button>
         </div>
         <div className="modal__content">
           {children}
